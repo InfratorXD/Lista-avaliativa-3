3c738cc15e99c61c7ee194e22863f77ab2b1fdf4
Guilherme Cazé <141370137+InfratorXD@users.noreply.github.com>
2023-11-21 09:14:12 -0300

    Update Questão 3.c

--- Questão 3.c
+++ Questão 3.c
@@ -1,52 +1,42 @@
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
     int main(){
-    char placa_letra[5];
-    int placa_digito;
     char dias[20];
+    char placa[9];
 
-
-
-
-scanf("%3s-%d", placa_letra, &placa_digito);  
-
+scanf("%s", placa);  
 
 while (getchar() != '\n');
 
 scanf(" %19[^\n]",dias);
   
-if (placa_digito < 0 || placa_digito > 9999 || strlen(placa_letra) != 3) {
-        printf("Placa invalida\n");
-        return 0;
-      }
 
-    
-
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa_digito %10 == 0 || placa_digito %10 == 1)){
-            printf("%s-%d nao pode circular segunda-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa[7] == '0' || placa[7] == '1')){
+            printf("%s nao pode circular segunda-feira\n",placa);
             return 0;
         }
         
    
-        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa_digito %10 == 2 || placa_digito %10 == 3)){
-            printf("%s-%d nao pode circular terca-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa[6] == '2' || placa[7] == '3')){
+            printf("%s nao pode circular terca-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa_digito %10 == 4 || placa_digito %10 == 5)){
-            printf("%s-%d nao pode circular quarta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa[6] == '4' || placa[7] == '5')){
+            printf("%s nao pode circular quarta-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa_digito %10 == 6 || placa_digito %10 == 7)){
-            printf("%s-%d nao pode circular quinta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa[6] == '6' || placa[7] == '7')){
+            printf("%s nao pode circular quinta-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa_digito %10 == 8 || placa_digito %10 == 9)){
-            printf("%s-%d nao pode circular sexta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa[6] == '8' || placa[7] == '9')){
+            printf("%s nao pode circular sexta-feira\n",placa);
             return 0;
         }
 
@@ -87,32 +77,37 @@ if (placa_digito < 0 || placa_digito > 9999 || strlen(placa_letra) != 3) {
             return 0;
         }
 
-            
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa_digito != 0 ||1)){
-            printf("%s-%d pode circular segunda-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa != 0 || 1)){
+            printf("%s pode circular segunda-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa_digito != 2 ||3)){
-            printf("%s-%d pode circular terca-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa != 2 ||3)){
+            printf("%s pode circular terca-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa_digito != 4 ||5)){
-            printf("%s-%d pode circular quarta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa != 4 || 5)){
+            printf("%s pode circular quarta-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa_digito != 6 ||7)){
-            printf("%s-%d pode circular quinta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa != 6 || 7)){
+            printf("%s pode circular quinta-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa_digito != 8 ||9)){
-            printf("%s-%d pode circular sexta-feira\n",placa_letra, placa_digito);
+        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa != 8 ||9)){
+            printf("%s pode circular sexta-feira\n",placa);
             return 0;
         }
 
+
+        //if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa[7] == != 0 ||placa[7]== != 1)){
+            //printf("%s pode circular segunda-feira\n",placa);
+           // return 0;
+        //}
+        
         return 0;
     }
         
