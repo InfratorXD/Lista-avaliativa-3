14b6b0cc8626ca52952f4467fa5eed00533b26ff
Guilherme Cazé <141370137+InfratorXD@users.noreply.github.com>
2023-11-24 11:11:00 -0300

    Update Questão 3.c

--- Questão 3.c
+++ Questão 3.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+//criar uma validação pros dias, como "QUINTA-FEIRA", "SEXTA-FEIRA"..., e puxar a função la pra baixo
 int Placas(char *placa) {
-
-    if (strlen(placa) == 8 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) && placa[3] == '-' && isdigit(placa[4]) && isdigit(placa[5]) && isdigit(placa[6]) && isdigit(placa[7])) {
-    return 1;
+    if (strlen(placa) == 8 &&
+        isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
+        placa[3] == '-' &&
+        isdigit(placa[4]) && isdigit(placa[5]) && isdigit(placa[6]) && isdigit(placa[7])) {
+        return 1;
     }
 
-    
-    if (strlen(placa) == 7 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) && isdigit(placa[3]) && isalpha(placa[4]) && isdigit(placa[5]) && isdigit(placa[6])) {
-    return 1;
+    if (strlen(placa) == 7 &&
+        isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
+        isdigit(placa[3]) && isalpha(placa[4]) && isdigit(placa[5]) && isdigit(placa[6])) {
+        return 1;
     }
 
     return 0;
 }
 
+
+
+void toUpperString(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = toupper((unsigned char)str[i]);
+    }
+}
+
 int main() {
     char dias[20];
-    char placa[10];  
+    char placa[12];
 
     fgets(placa, sizeof(placa), stdin);
-    placa[strcspn(placa, "\n")] = '\0';  
+    placa[strcspn(placa, "\n")] = '\0';
 
     fgets(dias, sizeof(dias), stdin);
     dias[strcspn(dias, "\n")] = '\0';
+
+    if (!Placas(placa)) {
+        printf("Placa invalida\n");
+        return 0;
+    }
+
+    //toUpperString(dias);
     
         if(!Placas(placa)&&(dias[0]== 's' && dias[1] == 'e' && dias[2] == 'g')) {
         printf("Placa invalida\n");
@@ -65,33 +83,34 @@ int main() {
 
     if(!Placas(placa)) {
         printf("Placa invalida\n");
-        return 1;
+        return 0;
     }
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa[7] == '0' || placa[7] == '1')){
-            printf("%s nao pode circular segunda-feira\n",placa);
-            return 0;
-        }
-        
-   
-        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa[6] == '2' || placa[7] == '3')){
-            printf("%s nao pode circular terca-feira\n",placa);
-            return 0;
-        }
+    char ultimo_caractere = placa[strlen(placa) - 1];
+  
+        if((dias[0] == 'S' && dias[1] == 'E' && dias[2] == 'G') && (ultimo_caractere == '0' || ultimo_caractere == '1')) {
+    printf("%s nao pode circular segunda-feira\n", placa);
+    return 0;
+}
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa[6] == '4' || placa[7] == '5')){
-            printf("%s nao pode circular quarta-feira\n",placa);
-            return 0;
-        }
+if((dias[0] == 'T' && dias[1] == 'E' && dias[2] == 'R') && (ultimo_caractere == '2' || ultimo_caractere == '3')) {
+    printf("%s nao pode circular terca-feira\n", placa);
+    return 0;
+}
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa[6] == '6' || placa[7] == '7')){
-            printf("%s nao pode circular quinta-feira\n",placa);
-            return 0;
-        }
+if((dias[0] == 'Q' && dias[1] == 'U' && dias[2] == 'A') && (ultimo_caractere == '4' || ultimo_caractere == '5')) {
+    printf("%s nao pode circular quarta-feira\n", placa);
+    return 0;
+}
 
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa[6] == '8' || placa[7] == '9')){
-            printf("%s nao pode circular sexta-feira\n",placa);
-            return 0;
-        }
+if((dias[0] == 'Q' && dias[1] == 'U' && dias[2] == 'I') && (ultimo_caractere == '6' || ultimo_caractere == '7')) {
+    printf("%s nao pode circular quinta-feira\n", placa);
+    return 0;
+}
+
+if((dias[0] == 'S' && dias[1] == 'E' && dias[2] == 'X') && (ultimo_caractere == '8' || ultimo_caractere == '9')) {
+    printf("%s nao pode circular sexta-feira\n", placa);
+    return 0;
+}
 
         if((dias[0]== 'S' && dias[1] == 'A' && dias[2] == 'B')){
             printf("Nao ha proibicao no fim de semana\n");
@@ -129,27 +148,27 @@ int main() {
             return 0;
         }
 
-        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (placa[6] != '0' && placa[7] != '1')){
+        if((dias[0]== 'S' && dias[1] == 'E' && dias[2] == 'G') && (ultimo_caractere != '0' && ultimo_caractere != '1')){
             printf("%s pode circular segunda-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (placa[6] != '2' && placa[7] !='3')){
+        if((dias[0]== 'T' && dias[1] == 'E' && dias[2] == 'R') && (ultimo_caractere != '2' && ultimo_caractere !='3')){
             printf("%s pode circular terca-feira\n",placa);
             return 0;
         }
 
-        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (placa[6] != '4' && placa[7] != '5')){
+        if((dias[0]== 'Q' && dias[1] == 'U' && dias[2] == 'A') && (ultimo_caractere != '4' && ultimo_caractere != '5')){
             printf("%s pode circular quarta-feira\n",placa);
             return 0;
         }
 
-        if ((dias[0] == 'Q' && dias[1] == 'U' && dias[2] == 'I') && (placa[6] != '6' && placa[7] != '7')) {
+        if ((dias[0] == 'Q' && dias[1] == 'U' && dias[2] == 'I') && (ultimo_caractere != '6' && ultimo_caractere != '7')) {
     printf("%s pode circular quinta-feira\n", placa);
     return 0;
 }
 
-if ((dias[0] == 'S' && dias[1] == 'E' && dias[2] == 'X') && (placa[6] != '8' && placa[7] != '9')) {
+if ((dias[0] == 'S' && dias[1] == 'E' && dias[2] == 'X') && (ultimo_caractere != '8' && ultimo_caractere != '9')) {
     printf("%s pode circular sexta-feira\n", placa);
     return 0;
 }
